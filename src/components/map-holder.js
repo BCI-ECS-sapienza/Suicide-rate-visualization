ce1e390693d95c73d68b90c32449b7d7bbb16206
Riccardo Bianchini <riccardo.bianchini96@gmail.com>
2021-05-23 16:07:27 +0200

    Settings parameters added to map-holder

--- src/components/map-holder.js
+++ src/components/map-holder.js
@@ -1,71 +1,109 @@
 // Width and Height of the whole visualization
-var width = document.getElementById('map-holder').offsetWidth;
-var height = document.getElementById('map-holder').offsetHeight;
+const width = document.getElementById('map-holder').offsetWidth;
+const height = document.getElementById('map-holder').offsetHeight;
 
 // Create SVG
-var map = d3.select( '#map-holder' )
-    .append( "svg" )
-    .attr( "width", '100%' )
-    .attr( "height", '100%' )
-    .attr('viewBox', ("0 0 "+ width + " " + height));
+const map = d3.select( '#map-holder' )
+  .append( "svg" )
+  .attr( "width", '100%' )
+  .attr( "height", '100%' )
+  .attr('viewBox', ("0 0 "+ width + " " + height));
 
-var path = d3.geoPath();
-var projection = d3.geoMercator()
-    .scale(100, 30)
-    .center([0, 10])
-    .translate([width/2, height/2]);
-
-
-// Load external data and boot
-/*
-d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  //.defer(d3.csv, "../../data/data.csv", function(d) { data.set(d.country, +d.suicides_pop); })
-  .await(ready);
-*/
+// Create path and projection
+const path = d3.geoPath();
+const projection = d3.geoMercator()
+  .scale(100, 30)
+  .center([0, 10])
+  .translate([width/2, height/2]);
 
 // Data and color scale
 //var data = d3.map();
-var colorScale = d3.scaleThreshold()
-  .domain([10000, 100000, 1000000, 3000000, 10000000]) //, 500000000])
-  .range(d3.schemeReds[5]);
+//var colorScale = d3.scaleThreshold()
+  //.domain([10000, 100000, 1000000, 3000000, 10000000]) //, 500000000])
+  //.range(d3.schemeReds[5]);
 
 function makeMap() {
-  var data = d3.map();
+  const dataYearLoaded = controller.getDataYear();
+  const dataFilteredLoaded = controller.getDataFiltered();
+
+  const dataYear = aggregateDataMap(dataYearLoaded);
+  const dataFiltered = aggregateDataMap(dataFilteredLoaded);
+  const colorLabel = 'Suicide ratio'
+
+  // mapping (country, #suicides) in data
+  const data = d3.map();
+  for(var i = 0; i<dataFiltered.length; i++){
+    data.set(dataFiltered[i].name, +dataFiltered[i].suicides);
+  }
+
+  // Data and color scale
+  //var data = d3.map();
+  const colorArray = controller.suicideColorScale;
+  const colorValue = d => d.value.suicides_pop;
+  const colorScale = d3.scaleThreshold()
+  //const colorScale = d3.scaleQuantize()
+        .domain(d3.extent(controller.getDataYear, colorValue))
+        .range(colorArray);
+
+  // Legend
+  const g = map.append("g")
+    .attr("class", "legendThreshold")
+    .attr("transform", "translate(20, 100)");
+  const labels = ['0', '1-5', '6-10', '11-25', '26-50'];
+  const legend = d3.legendColor().scale(colorScale)
+    .labelFormat(d3.format(".0f"))
+    .title(colorLabel);
+/*
+  const legend = d3.legendColor()
+    .labels(function (d) { return labels[d.i]; })
+    .shapePadding(4)
+    .scale(colorScale);*/
+  map.select(".legendThreshold")
+    .call(legend)
+      .attr("class","axis-text");
+  
+  // Tooltip
+  const tip = d3.select('#map-holder')
+    .append('tip')
+    .attr("id", "tooltip")
+    .attr("class", "tooltip")
+    .attr('style', 'opacity: 0;');
   
   d3.json(
     "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(json) {
 
     // Draw the map
-      map.append("g")
+    map.append("g")
       .selectAll("path")
       .data(json.features)
       .enter()
       .append("path")
-        // draw each country
+      // draw each country
       .attr("d", d3.geoPath()
         .projection(projection)
       )
       // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(data.code) || 0;
+      .attr("fill", function (d) {    
+        d.total = data.get(d.properties.name) || 0;
+        console.log(d.total);
         return colorScale(d.total);
       })
       .style("stroke", "transparent")
       .attr("class", function(d){ return "Country" } )
       .style("opacity", .8)
+      // adding event on mouseover
       .on("mouseover", function (d) {
         d3.select(this)
             .style("fill", '#80ced6')
             .style("cursor", "pointer");
         d3.select('#tooltip')
             .style("left", (d3.event.pageX + 15) + "px")
-			      .style("top", (d3.event.pageY - 28) + "px")
-			      .transition().duration(400)
-			      .style("opacity", 1)
-			      .text(d.id);
+            .style("top", (d3.event.pageY - 28) + "px")
+            .transition().duration(400)
+            .style("opacity", 1)
+            .text(d.properties.name + ': ' + d.total);
       })
+      // adding event on mouseout
       .on("mouseout", function(d){
         d3.select(this)
           .style("fill", colorScale(d.total));	
@@ -75,81 +113,23 @@ function makeMap() {
       })
   });
 }
-/*
-function ready(error, topo) {
-    // Draw the map
-    map.append("g")
-      .selectAll("path")
-      .data(topo.features)
-      .enter()
-      .append("path")
-        // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
-      })
-      .style("stroke", "transparent")
-      .attr("class", function(d){ return "Country" } )
-      .style("opacity", .8)
-      .on("mouseover", function (d) {
-        d3.select(this)
-            .style("fill", '#80ced6')
-            .style("cursor", "pointer");
-        d3.select('#tooltip')
-            .style("left", (d3.event.pageX + 15) + "px")
-			      .style("top", (d3.event.pageY - 28) + "px")
-			      .transition().duration(400)
-			      .style("opacity", 1)
-			      .text(d.id);
-      })
-      .on("mouseout", function(d){
-        d3.select(this)
-          .style("fill", colorScale(d.total));	
-        d3.select('#tooltip')
-          .transition().duration(300)
-          .style("opacity", 0);
-      })
-      .call(d3.zoom().on("zoom", function () {
-        map.attr("transform", d3.event.transform);
-     }))
-    }
-*/
-// Legend
-var rect = map.append('rect')
-    .attr("width", '100')
-    .attr("height", '120')
-    .style("fill", 'lightgrey')
-    .attr("transform", "translate(15, 80)");
-var g = map.append("g")
-    .attr("class", "legendThreshold")
-    .attr("transform", "translate(20, 100)");
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", 0)
-    .attr("y", -6)
-    .text("Suicide ratio");
 
-var labels = ['0', '1-5', '6-10', '11-25', '26-100', '101-1000', '> 1000'];
-var legend = d3.legendColor()
-    .labels(function (d) { return labels[d.i]; })
-    .shapePadding(4)
-    .scale(colorScale);
-map.select(".legendThreshold")
-    .call(legend);
-
-// Tooltip
-var tip = d3.select('#map-holder')
-    .append('tip')
-    .attr("id", "tooltip")
-    .attr("class", "tooltip")
-    .attr('style', 'opacity: 0;');
-
-  
-// draw graph on dataloaded
+// Function to manage data to be represented
+const aggregateDataMap = (dataMap) => {
+  const data = d3.nest()
+    .key((d) => d.country)
+    //.rollup((d) => Math.round(d3.sum(d, (g) => g.suicides_pop)))
+    .rollup((d) => Math.round(d3.mean(d, (g) => g.suicides_pop)))
+    .entries(dataMap)
+    .map(function(group){
+            return{
+                name: group.key,
+                suicides: group.value
+        };
+    })
+  return data;
+}
+// draw map on dataloaded
 controller.addListener('dataLoaded', function (e) {
   makeMap();
 });
