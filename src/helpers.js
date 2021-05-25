77f54edc42a15fee558a1d171410b67e01b8390f
Manuel Ivagnes <ivagnes.manuel@gmail.com>
2021-05-25 17:32:38 +0200

    year filter

--- src/helpers.js
+++ src/helpers.js
@@ -11,3 +11,36 @@ const yearSelected = () => {
     controller.triggerYearFilterEvent(selectedYear);
 };
 
+
+// data aggregators
+const aggregateDataByCountry = (dataIn) => {
+    const data = d3.nest()
+      .key( (d) => d.country)
+      .rollup( (d) =>  ({
+        suicides_pop: Math.round(d3.mean(d, (g) => g.suicides_pop)),
+        gdp_for_year: d[0].gdp_for_year,    // same for each category
+        gdp_per_capita: d[0].gdp_per_capita
+      }))
+    .entries(dataIn)
+    return data;
+};
+
+const aggregateDataByAge = (dataIn) => {
+    const data = d3.nest()
+      .key( (d) => d.age)
+      .rollup( (d) =>  ({
+        suicides_pop: Math.round(d3.mean(d, (g) => g.suicides_pop)),
+      }))
+    .entries(dataIn)
+    return data;
+  };
+
+const aggregateDataBySex = (dataIn) => {
+    const data = d3.nest()
+      .key( (d) => d.sex)
+      .rollup( (d) =>  ({
+        suicides_pop: Math.round(d3.mean(d, (g) => g.suicides_pop)),
+      }))
+    .entries(dataIn)
+    return data;
+  };
