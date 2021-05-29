ae0c20563dcd20e44de31ca7ebfe79e2e9380754
Riccardo Bianchini <riccardo.bianchini96@gmail.com>
2021-05-29 16:46:46 +0200

    Added radar chart bases

--- src/components/radarChart.js
+++ src/components/radarChart.js
@@ -1,138 +1,253 @@
-// Width and Height of the whole visualization
+// define variables
+var sides = 5;
+var levels = 4;
+
 var width = document.getElementById('radar-pca').offsetWidth;
 var height = document.getElementById('radar-pca').offsetHeight;
 
-// svg
-var svgRadar = d3.select("#radar-pca").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", "0 0 " + width + " " + height)
-    .attr("opacity", 0);
+var size = Math.min(width, height);
+
+var offset = Math.PI;
+var polyangle = (Math.PI*2)/sides;
+var r = 0.8 * size;
+var r_0 = r/2;
+
+var center = {
+  x: width/2,
+  y: height/2
+}
+
+var features = ['Country', 
+    'suicides ratio',
+    '#suicides',
+    'population',
+    'year gdp',
+    'capita gdp'
+  ];
+
+var ticks = generateTicks(levels);
+
+var scale = d3.scaleLinear()
+  .domain([0,100])
+  .range([0, r_0]);
+
+// define svg 
+var svgRadar = d3.select('#radar-pca')
+    .append('svg')
+    //.attr('opacity', 0)
+    .attr('width', width)
+    .attr('height', height);
+
+var g = svgRadar.append('g');
+
+// generate all the vertices of our polygon
+var points = [];
+var length = 100;
+for (var vertex = 0; vertex<sides; vertex++){
+  var theta = vertex * polyangle;
+  points.push(generatePoint(length, theta));
+}
+points = points.concat(points[0]);
+
+
+// draw the graph with data
+function drawRadar(dataYear){
+  
+  drawPath(points, g);
+  generateAndDrawLevels(levels, sides);
+  generateAndDrawLines(sides);
+  drawAxis(ticks, levels);
+  data = getData(dataYear);
+  //console.log(data);
+  //drawData(data, sides);
+}
+
+// define functions called above
+// function to get data
+function getData(dataYear){ 
+  var data = [];  
+  if(controller.selectedCountries.length > 0){
+      countries = []
+      
+      for(var i = 0; i<controller.selectedCountries.length; i++){
+          countries.push(controller.selectedCountries[i].id)
+      }
+      
+      for (var j = 0; j<dataYear.length; j++){
+          if(countries.includes(dataYear[j].key)){
+              data.push([{
+                  'Country': dataYear[j].key, 
+                  'suicides ratio': dataYear[j].value.suicides_pop,
+                  '#suicides': dataYear[j].value.suicides_no,
+                  'population': dataYear[j].value.population,
+                  'year gdp': dataYear[j].value.gdp_for_year,
+                  'capita gdp': dataYear[j].value.gdp_per_capita
+                }]);
+          }
+      }
+  } 
+  return data;  
+}
+
+// generate points
+function generatePoint(length, angle){
+  var point = {
+    x: center.x + (length * Math.sin(offset-angle)),
+    y: center.y + (length * Math.cos(offset -angle))
+  }
+  return point;
+}
+
+// generate and draw the path
+function drawPath(points, parent){
+  var lineGenerator = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+  
+  parent.append('path')
+    .attr('d', lineGenerator(points))
+    .attr('fill', 'none');
+}
+
+// generate and draw the polygon levels
+function generateAndDrawLevels(levelsCount, sidesCount){
+  
+  for(var level = 1; level<= levelsCount; level++){
     
-// set parameters
-var features = ['population', 'year gdp', 'capita gdp', 'suicides ratio', '#suicides']
-var colors = ['#d8b365', '#f5f5f5', '#5ab4ac'];
-
-setRadar();
-
-function updateRadar(dataFiltered){   
-    var data = [];
-    if(controller.selectedCountries.length > 0){
-        countries = []
-        for(var i = 0; i<controller.selectedCountries.length; i++){
-            countries.push(controller.selectedCountries[i].id)
-        }
-        for (var j = 0; j<dataFiltered.length; j++){
-            if(countries.includes(dataFiltered[j].key)){
-                
-                data.push([
-                    {axis:'suicides ratio',value:dataFiltered[j].value.gdp_per_capita},
-                    //{axis:'#suicides',value:dataFiltered[j].value.suicides_no},
-                    //{axis:'population',value:dataFiltered[j].value.population},
-                    {axis:'year gdp',value:dataFiltered[j].value.gdp_for_year},
-                    {axis:'capita gdp',value:dataFiltered[j].value.gdp_per_capita}
-                ]);
-            }
-        }
-    } 
-    console.log(data);  
+    var hyp = (level/levelsCount) * r_0;
+    var points = [];
+    
+    for(var vertex = 0; vertex<sidesCount; vertex++){
+      
+      const theta = vertex * polyangle;
+      points.push(generatePoint(hyp, theta));
+    }
+    const group = g.append('g')
+      .attr('class', 'levels')
+      .attr('fill', 'none')
+      .attr('stroke', 'lightgray');
+    points = points.concat(points[0]);
+    
+    drawPath(points, group);
+  }
 }
 
-function setRadar(){
-    var dataRadar = [];
-    //var features = ["A","B","C","D","E","F"];
-    //generate the dataRadar
-    for (var i = 0; i < 3; i++){
-        var point = {}
-        //each feature will be a random number from 1-9
-        features.forEach(f => point[f] = 1 + Math.random() * 8);
-        dataRadar.push(point);
-    }
-
-    // Plotting the gridlines
-    var radialScale = d3.scaleLinear()
-        .domain([0,10])
-        .range([0,115]);
-    var ticks = [2,4,6,8,10];
-
-    // Add circles
-    ticks.forEach(t =>
-    svgRadar.append("circle")
-        .attr("cx", width/2)
-        .attr("cy", height/2)
-        .attr("fill", "none")
-        .attr("stroke", "gray")
-        .attr("r", radialScale(t))
-    );
-
-    // tick labels
-    ticks.forEach(t =>
-    svgRadar.append("text")
-        .attr("x", width/2+5)
-        .attr("y", height/2 - radialScale(t))
-        .text(t.toString())
-        .style("font-size", "10px")
-        .style("fill", "gray")
-    );
-
-    // Plotting the axes
-    function angleToCoordinate(angle, value){
-    var x = Math.cos(angle) * radialScale(value);
-    var y = Math.sin(angle) * radialScale(value);
-    return {"x": width/2 + x, "y": height/2 - y};
-    }
-
-    for (var i = 0; i < features.length; i++) {
-    var ft_name = features[i];
-    var angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
-    var line_coordinate = angleToCoordinate(angle, 10);
-    var label_coordinate = angleToCoordinate(angle, 10.5);
-
-    //draw axis line
-    svgRadar.append("line")
-        .attr("x1", width/2)
-        .attr("y1", height/2)
-        .attr("x2", line_coordinate.x)
-        .attr("y2", line_coordinate.y)
-        .attr("stroke","gray");
-
-    //draw axis label
-    svgRadar.append("text")
-        .attr("x", label_coordinate.x)
-        .attr("y", label_coordinate.y)
-        .text(ft_name)
-        .style("fill", "gray");
-    }
-
-    // Plotting dataRadar
-    var line = d3.line()
-        .x(d => d.x)
-        .y(d => d.y);
-        
-
-    function getPathCoordinates(dataRadar_point){
-    var coordinates = [];
-    for (var i = 0; i < features.length; i++){
-        var ft_name = features[i];
-        var angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
-        coordinates.push(angleToCoordinate(angle, dataRadar_point[ft_name]));
-    }
-    return coordinates;
-    }
-
-    for (var i = 0; i < dataRadar.length; i ++){
-    let d = dataRadar[i];
-    let color = colors[i];
-    let coordinates = getPathCoordinates(d);
-
-    //draw the path element
-    svgRadar.append("path")
-        .datum(coordinates)
-        .attr("d",line)
-        .attr("stroke-width", 3)
-        .attr("stroke", color)
-        .attr("fill", color)
-        .attr("stroke-opacity", 1)
-        .attr("opacity", 0.5);
-    }
+// generate and draw the lines extending from the center
+function generateAndDrawLines(sidesCount){
+  var group = g.append('g')
+    .attr('class', 'grid-lines')
+    .attr('stroke', 'lightgreen');
+  
+  for(var vertex = 1; vertex<=sidesCount; vertex++){
+    var theta = vertex * polyangle;
+    var point = generatePoint(r_0, theta);
+    drawPath([center, point], group);
+  }
 }
+// NOTE: for now never called
+function generateTicks(){
+  const ticks = [];
+  var step = 100/levels;
+  
+  for(var i=0; i<=levels; i++){
+    var num = step*i;
+    
+    if(Number.isInteger(step)){
+      ticks.push(num);
+    }
+    else{
+      ticks.push(num.toFixed(2));
+    }
+
+  }
+  return ticks;
+}
+
+// generate and draw the chart axis
+function drawText( text, point, isAxis, group ){
+  if ( isAxis )
+  {
+      //var xSpacing = text.toString().includes( "." ) ? 30 : 22;
+      var xSpacing = 10;
+      group.append( "text" )
+          .attr( "x", point.x - xSpacing )
+          .attr( "y", point.y - 3 )
+          .html( text )
+          .style( "text-anchor", "middle" )
+          .attr( "fill", "darkgrey" )
+          .style( "font-size", "12px" )
+          .style( "font-family", "sans-serif" )
+          .style('opacity', .7);
+  }
+  else
+  {
+      group.append( "text" )
+          .attr( "x", point.x )
+          .attr( "y", point.y )
+          .html( text )
+          .style( "text-anchor", "middle" )
+          .attr( "fill", "darkgrey" )
+          .style( "font-size", "12px" )
+          .style( "font-family", "sans-serif" )
+          .style('opacity', .7);
+  }
+
+};
+
+function drawAxis( ticks, levelsCount ){
+    var groupL = g.append( "g" ).attr( "class", "tick-lines" );
+    var point = generatePoint(r_0, 0);
+    drawPath( [ center, point ], groupL );
+
+    var groupT = g.append( "g" ).attr( "class", "ticks" );
+
+    ticks.forEach( ( d, i ) =>
+    {
+        var r = ( i / levelsCount ) * r_0;
+        var p = generatePoint(r, 0);
+        var points =
+            [
+                p,
+                {
+                    ...p,
+                    x: p.x -10
+                }
+
+            ];
+        drawPath( points, groupL );
+        drawText( d, p, true, groupT );
+    } );
+};
+
+// draw data
+function drawCircles(points){
+    g.append( "g" )
+        .attr( "class", "indic" )
+        .selectAll( "circle" )
+        .data( points )
+        .enter()
+        .append( "circle" )
+        .attr( "cx", d => d.x )
+        .attr( "cy", d => d.y )
+        .attr( "r", 8 );
+};
+
+function drawData(dataset, n ){
+    var points = [];
+    dataset.forEach( ( d, i ) => 
+    {
+        var len = scale( d.value );
+        var theta = i * ( 2 * Math.PI / n );
+
+        points.push(
+            {
+                ...generatePoint(len, theta),
+                value: d.value
+            } );
+    } );
+
+    var group = g.append( "g" ).attr( "class", "shape" );
+
+    drawPath( [ ...points, points[ 0 ] ], group );
+    drawCircles( points );
+};
