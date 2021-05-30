23f9dc888bfc94ce9d993885ae5556d148fe0a36
Riccardo Bianchini <riccardo.bianchini96@gmail.com>
2021-05-30 16:53:48 +0200

    Added data to the radar chart

--- src/components/radarChart.js
+++ src/components/radarChart.js
@@ -1,50 +1,42 @@
-// define variables
-var sides = 5;
-var levels = 4;
+// define parameters
+const sides = 5;
+const levels = 4;
 
-var width = document.getElementById('radar-pca').offsetWidth;
-var height = document.getElementById('radar-pca').offsetHeight;
+let width = document.getElementById('radar-pca').offsetWidth;
+let height = document.getElementById('radar-pca').offsetHeight;
 
-var size = Math.min(width, height);
+let size = Math.min(width, height);
 
-var offset = Math.PI;
-var polyangle = (Math.PI*2)/sides;
-var r = 0.8 * size;
-var r_0 = r/2;
+const offset = Math.PI;
+const polyangle = (Math.PI*2)/sides;
+let r = 0.8 * size;
+let r_0 = r/2;
 
-var center = {
+let center = {
   x: width/2,
   y: height/2
 }
+const selectedColors = ['#d01c8b',
+    '#f1b6da',
+    '#b8e186',
+    '#4dac26'];
 
-var features = ['Country', 
-    'suicides ratio',
-    '#suicides',
-    'population',
-    'year gdp',
-    'capita gdp'
-  ];
-
-var ticks = generateTicks(levels);
-
-var scale = d3.scaleLinear()
-  .domain([0,100])
-  .range([0, r_0]);
+const ticks = generateTicks(levels);
 
 // define svg 
-var svgRadar = d3.select('#radar-pca')
+const svgRadar = d3.select('#radar-pca')
     .append('svg')
-    //.attr('opacity', 0)
+    .attr('opacity', 0)
     .attr('width', width)
     .attr('height', height);
 
-var g = svgRadar.append('g');
+const g = svgRadar.append('g');
 
 // generate all the vertices of our polygon
-var points = [];
-var length = 100;
-for (var vertex = 0; vertex<sides; vertex++){
-  var theta = vertex * polyangle;
+let points = [];
+const length = 100;
+for (let vertex = 0; vertex<sides; vertex++){
+  let theta = vertex * polyangle;
   points.push(generatePoint(length, theta));
 }
 points = points.concat(points[0]);
@@ -52,46 +44,67 @@ points = points.concat(points[0]);
 
 // draw the graph with data
 function drawRadar(dataYear){
-  
+
+  const feature_scale = getMaxFeatures();
   drawPath(points, g);
   generateAndDrawLevels(levels, sides);
   generateAndDrawLines(sides);
   drawAxis(ticks, levels);
   data = getData(dataYear);
-  //console.log(data);
+  myDrawData(data, sides, feature_scale);
   //drawData(data, sides);
 }
 
 // define functions called above
 // function to get data
 function getData(dataYear){ 
-  var data = [];  
+  let data = [];  
   if(controller.selectedCountries.length > 0){
       countries = []
       
-      for(var i = 0; i<controller.selectedCountries.length; i++){
+      for(let i = 0; i<controller.selectedCountries.length; i++){
           countries.push(controller.selectedCountries[i].id)
       }
       
-      for (var j = 0; j<dataYear.length; j++){
+      for (let j = 0; j<dataYear.length; j++){
           if(countries.includes(dataYear[j].key)){
-              data.push([{
-                  'Country': dataYear[j].key, 
+              data.push({
+                  'country': dataYear[j].key, 
                   'suicides ratio': dataYear[j].value.suicides_pop,
                   '#suicides': dataYear[j].value.suicides_no,
                   'population': dataYear[j].value.population,
-                  'year gdp': dataYear[j].value.gdp_for_year,
-                  'capita gdp': dataYear[j].value.gdp_per_capita
-                }]);
+                  'year_gdp': dataYear[j].value.gdp_for_year,
+                  'capita_gdp': dataYear[j].value.gdp_per_capita
+                });
           }
       }
   } 
   return data;  
 }
 
+// extracting max value for each feature in the selected data
+function getMaxFeatures(){
+  const dataYear = aggregateDataByCountryRadar(controller.dataAll);
+  
+  const max_suicides_pop = d3.max(dataYear, d => d.value.suicides_pop );
+  const max_suicides_no = d3.max(dataYear, d => d.value.suicides_no);
+  const max_population = d3.max(dataYear, d => d.value.population);
+  const max_gdp_for_year = d3.max(dataYear, d => d.value.gdp_for_year);
+  const max_gdp_per_capita = d3.max(dataYear, d => d.value.gdp_per_capita);
+
+  const feature_scale = {
+    'suicides ratio': max_suicides_pop,
+    '#suicides': max_suicides_no,
+    'population': max_population,
+    'year_gdp': max_gdp_for_year,
+    'capita_gdp': max_gdp_per_capita
+  };
+  return feature_scale;
+}
+
 // generate points
 function generatePoint(length, angle){
-  var point = {
+  const point = {
     x: center.x + (length * Math.sin(offset-angle)),
     y: center.y + (length * Math.cos(offset -angle))
   }
@@ -100,7 +113,7 @@ function generatePoint(length, angle){
 
 // generate and draw the path
 function drawPath(points, parent){
-  var lineGenerator = d3.line()
+  const lineGenerator = d3.line()
     .x(d => d.x)
     .y(d => d.y);
   
@@ -112,12 +125,12 @@ function drawPath(points, parent){
 // generate and draw the polygon levels
 function generateAndDrawLevels(levelsCount, sidesCount){
   
-  for(var level = 1; level<= levelsCount; level++){
+  for(let level = 1; level<= levelsCount; level++){
     
-    var hyp = (level/levelsCount) * r_0;
-    var points = [];
+    const hyp = (level/levelsCount) * r_0;
+    let points = [];
     
-    for(var vertex = 0; vertex<sidesCount; vertex++){
+    for(let vertex = 0; vertex<sidesCount; vertex++){
       
       const theta = vertex * polyangle;
       points.push(generatePoint(hyp, theta));
@@ -134,12 +147,12 @@ function generateAndDrawLevels(levelsCount, sidesCount){
 
 // generate and draw the lines extending from the center
 function generateAndDrawLines(sidesCount){
-  var group = g.append('g')
+  const group = g.append('g')
     .attr('class', 'grid-lines')
     .attr('stroke', 'lightgreen');
   
-  for(var vertex = 1; vertex<=sidesCount; vertex++){
-    var theta = vertex * polyangle;
+  for(let vertex = 1; vertex<=sidesCount; vertex++){
+    const theta = vertex * polyangle;
     var point = generatePoint(r_0, theta);
     drawPath([center, point], group);
   }
@@ -147,10 +160,10 @@ function generateAndDrawLines(sidesCount){
 // NOTE: for now never called
 function generateTicks(){
   const ticks = [];
-  var step = 100/levels;
+  const step = 100/levels;
   
-  for(var i=0; i<=levels; i++){
-    var num = step*i;
+  for(let i=0; i<=levels; i++){
+    const num = step*i;
     
     if(Number.isInteger(step)){
       ticks.push(num);
@@ -167,8 +180,8 @@ function generateTicks(){
 function drawText( text, point, isAxis, group ){
   if ( isAxis )
   {
-      //var xSpacing = text.toString().includes( "." ) ? 30 : 22;
-      var xSpacing = 10;
+      //const xSpacing = text.toString().includes( "." ) ? 30 : 22;
+      const xSpacing = 10;
       group.append( "text" )
           .attr( "x", point.x - xSpacing )
           .attr( "y", point.y - 3 )
@@ -195,17 +208,17 @@ function drawText( text, point, isAxis, group ){
 };
 
 function drawAxis( ticks, levelsCount ){
-    var groupL = g.append( "g" ).attr( "class", "tick-lines" );
-    var point = generatePoint(r_0, 0);
+    const groupL = g.append( "g" ).attr( "class", "tick-lines" );
+    const point = generatePoint(r_0, 0);
     drawPath( [ center, point ], groupL );
 
-    var groupT = g.append( "g" ).attr( "class", "ticks" );
+    const groupT = g.append( "g" ).attr( "class", "ticks" );
 
     ticks.forEach( ( d, i ) =>
     {
-        var r = ( i / levelsCount ) * r_0;
-        var p = generatePoint(r, 0);
-        var points =
+        const r = ( i / levelsCount ) * r_0;
+        const p = generatePoint(r, 0);
+        const points =
             [
                 p,
                 {
@@ -220,7 +233,7 @@ function drawAxis( ticks, levelsCount ){
 };
 
 // draw data
-function drawCircles(points){
+function drawCircles(points, color){
     g.append( "g" )
         .attr( "class", "indic" )
         .selectAll( "circle" )
@@ -229,15 +242,16 @@ function drawCircles(points){
         .append( "circle" )
         .attr( "cx", d => d.x )
         .attr( "cy", d => d.y )
-        .attr( "r", 8 );
+        .attr( "r", 4 )
+        .style('fill', color);
 };
 
-function drawData(dataset, n ){
-    var points = [];
+function drawData(dataset, n){
+    let points = [];
     dataset.forEach( ( d, i ) => 
     {
-        var len = scale( d.value );
-        var theta = i * ( 2 * Math.PI / n );
+        const len = scale( d.value );
+        const theta = i * ( 2 * Math.PI / n );
 
         points.push(
             {
@@ -246,8 +260,45 @@ function drawData(dataset, n ){
             } );
     } );
 
-    var group = g.append( "g" ).attr( "class", "shape" );
+    let group = g.append( "g" ).attr( "class", "shape" );
 
     drawPath( [ ...points, points[ 0 ] ], group );
     drawCircles( points );
 };
+
+function myDrawData(dataset, n, feature_scale){
+
+  // loop on elements of array
+  for(let el in dataset){
+    
+    //console.log(dataset[el]);
+    let points = [];
+    let i = 0;
+    
+    // loop on elements of dictionary
+    for(let key in dataset[el]){
+      //console.log(dataset[el][key]);
+      
+      if(key != 'country'){
+        //console.log(feature_scale[key]);
+        let scale = d3.scaleLinear()
+          .domain([0, feature_scale[key]])
+          .range([0, r_0]);
+        const theta = i * ( 2 * Math.PI / n );
+        const len = scale(dataset[el][key]);
+        i++;
+        points.push(
+          {
+              ...generatePoint(len, theta),
+              value: dataset[el][key]
+          });
+      }
+    }
+    let group = g.append( "g" ).attr( "class", "shape" );
+    
+    points = points.concat(points[0]);
+    
+    drawPath(points, group);
+    drawCircles(points, selectedColors[el]);
+  }
+}
